76ba7a0ef6f96def3bde505571b1702267d807b6
Pedro Osnaghi <pedroosnaghi@MacBook-Pro-M4-Max-de-Pedro.local>
2025-07-18 09:26:53 -0300

    Initial commit

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import GUI from "lil-gui";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { RGBELoader } from "three/addons/loaders/RGBELoader.js";
-
 import { EffectComposer } from "three/addons/postprocessing/EffectComposer.js";
 import { RenderPass } from "three/addons/postprocessing/RenderPass.js";
 import { UnrealBloomPass } from "three/addons/postprocessing/UnrealBloomPass.js";
